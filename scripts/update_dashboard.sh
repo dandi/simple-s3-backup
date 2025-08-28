a8da4cda624d1949ec0a6c298dec08f66922008e
Cody Baker <cody.c.baker.phd@gmail.com>
2025-08-28 14:02:17 -0400

    chore: remove diffs

--- scripts/update_dashboard.sh
+++ scripts/update_dashboard.sh
@@ -10,14 +10,6 @@ conda activate /orcd/data/dandi/001/s3-backup-environment
 
 cd /orcd/data/dandi/001/backup-status
 git pull
-mkdir diffs
-for x in {0..1}; do
-  mkdir diffs/$x
-  s5cmd --dry-run cp --if-size-differ s3://dandiarchive/blobs/$x* . | awk '{print $3}' | sort > diffs/$x/size_difference.txt
-  # mtimes on MIT cluster are date of copy from bucket, not mtime of file on bucket
-  # TODO: test if mtime on bucket is upload time or actual file mtime
-  s5cmd --dry-run cp --if-source-newer s3://dandiarchive/blobs/$x* . | awk '{print $3}' | sort > diffs/$x/newer_on_bucket.txt
-done
 s3backup dandi dashboard
 git add .
 git commit --message "update"
