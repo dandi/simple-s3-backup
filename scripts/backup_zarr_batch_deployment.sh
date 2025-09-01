ddd24e41bece54c1ef05eff2c72bfe4dd5d9edf4
Cody Baker <cody.c.baker.phd@gmail.com>
2025-09-01 14:54:50 -0400

    feat: add zarr batch

--- scripts/backup_zarr_batch_deployment.sh
+++ scripts/backup_zarr_batch_deployment.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition mit_normal
 #SBATCH --output /dev/null
-#SBATCH --array 0-255
+#SBATCH --array 0-5
 
 source /etc/profile.d/modules.sh  # When run via crontab, this is needed to load the modules
 module load miniforge
 
 conda activate /orcd/data/dandi/001/s3-backup-environment
 
-s3backup dandi blobs $SLURM_ARRAY_TASK_ID
+s3backup dandi zarr $SLURM_ARRAY_TASK_ID
