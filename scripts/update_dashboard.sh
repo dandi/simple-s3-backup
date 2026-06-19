ac25b6371a3db99c3b15a53af07e375528f2bc84
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-06-19 15:03:20 -0400

    Remove unnecessary blank line in update_dashboard.sh

--- scripts/update_dashboard.sh
+++ scripts/update_dashboard.sh
@@ -7,7 +7,6 @@
 
 source /etc/profile.d/modules.sh  # When run via crontab, this is needed to load the modules
 module load miniforge
-
 conda activate /orcd/data/dandi/001/s3-backup-environment
 
 cd /orcd/data/dandi/001/backup-status
